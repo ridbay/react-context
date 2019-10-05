37b3d86c6af573205401525a8df341f0e60d7f83
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-05 06:59:32 +0100

    react context api

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+// My app's Firebase configuration
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
-};
+    apiKey: "AIzaSyClPwh2ERccocYw-zundyAUwJk2f3Aso64",
+    authDomain: "ridbay-shop-db.firebaseapp.com",
+    databaseURL: "https://ridbay-shop-db.firebaseio.com",
+    projectId: "ridbay-shop-db",
+    storageBucket: "",
+    messagingSenderId: "750310140637",
+    appId: "1:750310140637:web:da3e1984d683dba0b20934"
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -39,11 +39,53 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
@@ -8,28 +9,23 @@ import CollectionsContext from '../../contexts/collections/collections.context'
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }
-      }
-
-    </CollectionsContext.Consumer>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
