5512d250e3647767b45625df6204f48075106e0e
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-05 08:59:31 +0100

    react context api

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,33 +15,34 @@ import './header.styles.scss';
 
 const Header = () => {
   const currentUser = useContext(CurrentUserContext);
-  const {hidden} = useContext(CartContext);
+  const { hidden } = useContext(CartContext);
 
-  return (<div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-    </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-    </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-      </div>
-      ) : (
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
           <Link className='option' to='/signin'>
             SIGN IN
-      </Link>
+          </Link>
         )}
         <CartIcon />
+      </div>
+      {hidden ? null : <CartDropdown />}
     </div>
-    {hidden ? null : <CartDropdown />}
-  </div>)
-
+  );
 };
 
 export default Header;
